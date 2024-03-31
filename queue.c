e49ba6705c497845120222d5710579ab2fec2016
96121503 <N96121503@gs,ncku,edu,tw>
2024-03-31 17:15:58 +0800

    Modified q_reverse()

--- queue.c
+++ queue.c
@@ -154,15 +154,10 @@ bool q_delete_dup(struct list_head *head)
 /* Swap every two adjacent nodes */
 void q_swap(struct list_head *head)
 {
-    struct list_head *node1, *node2;
-    // for (node1 = head->next, node2 = node1->next;
-    //      node1 != head && node2 != head;
-    //      node1 = node2->next, node2 = node1->next) {
-    //     list_del(node1);
-    //     list_add(node1, node2);
-    // }
-    for (node1 = head, node2 = node1->next; node1 != head && node2 != head;
-         node1 = node2->next, node2 = node1->next) {
+    struct list_head *node1, *node2, *safe;
+    for (node1 = head->next, node2 = node1->next, safe = node2->next;
+         node1 != head && node2 != head;
+         node1 = safe, node2 = node1->next, safe = node2->next) {
         list_del(node1);
         list_add(node1, node2);
     }
